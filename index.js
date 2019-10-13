1d19b17d89a7bf64b0fc61b21264723b3a8375d9
Timothy Hedge <timwhedge@gmail.com>
2019-10-13 15:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var mi = [];
+  var m;
+  for (m = 0; m < musicians.length; m++) {
+    mi.push(`${musicians[m]} plays ${instruments[m]}`)
+  }
+  return mi
+}
+
+function johnLennonFacts(jlfacts) {
+  var jlfex = [];
+  var f = 0;
+  while (f < jlfacts.length) {
+    jlfex.push(`${jlfacts[f]}!!!`);
+    f++;
+  }
+  return jlfex
+}
+
+function iLoveTheBeatles(num)
